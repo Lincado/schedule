82d8909c66c00b40f2cb5fcbd78cb2c94045f087
Leo Alves <leoalves0698@gmail.com>
2023-09-04 21:10:28 -0300

    feat: add login controlller

--- routes.js
+++ routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
 route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
 
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
-
+// Rotas de login
+route.get("/login/index", loginController.index) // toda página inicial dessa rota se chamara index
 
 module.exports = route;
