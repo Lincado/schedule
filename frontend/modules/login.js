8ecb50f9b68f8eb9a53a9ef1d81d63b85005112b
Leo Alves <leoalves0698@gmail.com>
2023-09-13 17:42:51 -0300

    fix: fixed login/registration validation

--- frontend/modules/login.js
+++ frontend/modules/login.js
@@ -17,19 +17,39 @@ export default class Login {
     }
     validate(e) {
         const el = e.target;
+        const emailDiv = el.querySelector(".form-group")
+        const passwordDiv = el.querySelector(".password")
         const emailInput = el.querySelector("input[name='email']");
         const passwordInput = el.querySelector("input[name='password']");
         let error = false;
+        
 
         if(!validator.isEmail(emailInput.value))  {
-            alert("E-mail inválido");
+            const p = this.createTagAndMsgAlert("E-mail inválido");
+            emailDiv.append(p)
             error = true
+        } else {
+            const existingAlert = emailDiv.querySelector(".alert-danger")
+            if(existingAlert) emailDiv.remove(existingAlert)
         }
 
         if(passwordInput.value.length < 3 || passwordInput.value.length > 50) {
-            alert("Senha precisa ter entre 3 a 40 caracteres")
+            const p = this.createTagAndMsgAlert("Senha precisa ter entre 3 a 40 caracteres");
+            passwordDiv.append(p)
             error = true
+        } else {
+            const existingAlert = passwordDiv.querySelector(".alert-danger")
+            if(existingAlert) passwordDiv.remove(existingAlert)
         }
+
         if(!error) el.submit()
     }
+
+    createTagAndMsgAlert(msg) {
+        const p = document.createElement("p");
+        p.classList.add("alert", "alert-danger", "mt-1");
+        p.innerText = msg
+        return p
+    }
+
 }
